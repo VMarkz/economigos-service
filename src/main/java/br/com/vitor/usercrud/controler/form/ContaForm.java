c5fa9824a07f8697dd76f2089551b258c6bd09ca
VMarkz <vitor.portela@bandtec.com.br>
2021-03-26 17:54:54 -0300

    iniciando o desenvolvimento dos endpoints para a classe Conta

---
+++ src/main/java/br/com/vitor/usercrud/controler/form/ContaForm.java
@@ -0,0 +1,23 @@
+package br.com.vitor.usercrud.controler.form;
+
+import br.com.vitor.usercrud.model.Conta;
+import com.sun.istack.NotNull;
+
+import javax.validation.constraints.NotEmpty;
+
+public class ContaForm implements CommonForm{
+
+    @NotNull @NotEmpty
+    private String banco;
+    @NotNull @NotEmpty
+    private String numeroConta;
+    @NotNull @NotEmpty
+    private String descricao;
+    @NotNull @NotEmpty
+    private String apelido;
+
+    @Override
+    public Conta converter() {
+        return (new Conta(this.banco, this.numeroConta, this.descricao, this.apelido));
+    }
+}
