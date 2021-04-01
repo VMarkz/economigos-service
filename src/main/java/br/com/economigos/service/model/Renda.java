18b1f7c165a877a8ed328a973100eba27ef2d0fb
MarkzBandtec <vitor.portela@bandtec.com.br>
2021-04-01 09:23:12 -0300

    adcionando recebido à classe Renda

--- src/main/java/br/com/economigos/service/model/Renda.java
+++ src/main/java/br/com/economigos/service/model/Renda.java
@@ -9,9 +9,11 @@ public class Renda extends Contabil{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Boolean recebido;
 
-    public Renda(Double valor, String descricao, Boolean fixo, Categoria categoria, Conta conta) {
+    public Renda(Double valor, String descricao, Boolean fixo, Categoria categoria, Conta conta, Boolean recebido) {
         super(valor, descricao, fixo, categoria);
+        this.recebido = recebido;
     }
 
     public Renda() {
@@ -23,5 +25,13 @@ public class Renda extends Contabil{
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Boolean getRecebido() {
+        return recebido;
+    }
+
+    public void setRecebido(Boolean recebido) {
+        this.recebido = recebido;
     }
 }
