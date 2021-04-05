e80909f037d92fdb4d71a0affbcef976aea9f02a
VMarkz <vitor.portela@bandtec.com.br>
2021-04-05 19:40:22 -0300

    detalhamento de usuário está trazendo suas contas

--- src/main/java/br/com/economigos/service/controler/dto/DetalhesUsuarioDto.java
+++ src/main/java/br/com/economigos/service/controler/dto/DetalhesUsuarioDto.java
@@ -3,17 +3,20 @@ package br.com.economigos.service.controler.dto;
 import br.com.economigos.service.model.Usuario;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class DetalhesUsuarioDto {
 
     private Long id;
     private String email;
     private LocalDateTime dataCriacao;
+    private List<ContaDto> contaDtos;
 
     public DetalhesUsuarioDto(Usuario usuario) {
         this.id = usuario.getId();
         this.email = usuario.getEmail();
         this.dataCriacao = usuario.getDataCriacao();
+        this.contaDtos = ContaDto.converter(usuario.getContas());
     }
 
     public Long getId() {
@@ -38,5 +41,13 @@ public class DetalhesUsuarioDto {
 
     public void setDataCriacao(LocalDateTime dataCriacao) {
         this.dataCriacao = dataCriacao;
+    }
+
+    public List<ContaDto> getContaDtos() {
+        return contaDtos;
+    }
+
+    public void setContaDtos(List<ContaDto> contaDtos) {
+        this.contaDtos = contaDtos;
     }
 }
