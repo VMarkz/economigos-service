e128734705aafb464da2bd092f6bd98745e3b0c5
VMarkz <vitor.portela@bandtec.com.br>
2021-04-15 17:37:05 -0300

    implementando ListaObj no extrato (arquivo CSV)

--- src/main/java/br/com/economigos/service/controler/FileControler.java
+++ src/main/java/br/com/economigos/service/controler/FileControler.java
@@ -4,8 +4,8 @@ import br.com.economigos.service.model.Gasto;
 import br.com.economigos.service.model.Renda;
 import br.com.economigos.service.repository.GastoRepository;
 import br.com.economigos.service.repository.RendaRepository;
-import br.com.economigos.service.utils.CsvHelper;
 import br.com.economigos.service.utils.FileIo;
+import br.com.economigos.service.utils.ListaObj;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
@@ -35,45 +35,30 @@ public class FileControler {
     @Autowired
     GastoRepository gastoRepository;
 
-
     @GetMapping("/export")
     public ResponseEntity<Resource> getFile() throws IOException {
-        String filename = "Rendas&Gastos.csv";
+        String filename = "rendas&gastos.csv";
+
         List<Renda> rendas = rendaRepository.findAll();
+        ListaObj<Renda> listaObjRendas = new ListaObj(rendas.size());
+        for (Renda renda : rendas) {
+            listaObjRendas.adiciona(renda);
+        }
+
         List<Gasto> gastos = gastoRepository.findAll();
+        ListaObj<Gasto> listaObjGastos = new ListaObj(gastos.size());
+        for (Gasto gasto : gastos) {
+            listaObjGastos.adiciona(gasto);
+        }
+
         FileIo.createFile();
-        FileIo.separarGastosRendas(rendas, gastos);
+        FileIo.separarGastosRendas(listaObjRendas, listaObjGastos);
         InputStreamResource resource = new InputStreamResource(new FileInputStream(FileIo.getFile()));
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
                 .contentType(MediaType.parseMediaType("application/csv"))
                 .body(resource);
-    }
-
-    @GetMapping("/download/rendas")
-    public ResponseEntity<Resource> getCsvRendas() {
-        List<Renda> rendas = rendaRepository.findAll();
-        String filename = "rendas.csv";
-        InputStreamResource file = new InputStreamResource(CsvHelper.rendasToCSV(rendas));
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
-                .contentType(MediaType.parseMediaType("application/csv"))
-                .body(file);
-    }
-
-    @GetMapping("/download/gastos")
-    public ResponseEntity<Resource> getCsvGastos() {
-        List<Gasto> gastos = gastoRepository.findAll();
-        String filename = "gastos.csv";
-        InputStreamResource file = new InputStreamResource(CsvHelper.gastosToCSV(gastos));
-        System.out.println(file);
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
-                .contentType(MediaType.parseMediaType("application/csv"))
-                .body(file);
     }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,7 @@ INSERT INTO CARTAO(fechamento,limite,nome,pago,valor,vencimento, usuario_id) VAL
 INSERT INTO META(descricao, meta_gasto, nome, valor_inicial, valor_final,usuario_id) VALUES ('Viagem final de ano', true, 'Viagem', 0.00, 4000.00, 1);
 INSERT INTO CONTATO(email, mensagem) VALUES ('usuario@email.com', 'Gostei muito do app de vocês!');
 INSERT INTO RENDA(id,data_pagamento, descricao, fixo, tipo, valor, categoria_id, conta_id,recebido) VALUES (1,'2021-03-01', 'Meu salário', true, 'Renda', 2000.00, 5, 1, true);
-INSERT INTO GASTO(id,data_pagamento, descricao, fixo, tipo, valor, categoria_id, conta_id, pago, cartao_id) VALUES (2,'2021-05-01', 'Netflix', false, 'Gasto', 32.90,6, 1, true, 1);
+-- INSERT INTO GASTO(id,data_pagamento, descricao, fixo, tipo, valor, categoria_id, conta_id, pago, cartao_id) VALUES (2,'2021-05-01', 'Netflix', false, 'Gasto', 32.90,6, 1, true, 1);
 INSERT INTO RENDA(id,data_pagamento, descricao, fixo, tipo, valor, categoria_id, conta_id,recebido) VALUES (3,'2021-04-01', 'Meu investimento', false, 'Renda', 2000.00, 7, 1, true);
 INSERT INTO GASTO(id,data_pagamento, descricao, fixo, tipo, valor, categoria_id, conta_id, pago, cartao_id) VALUES (4,'2021-05-01', 'Spotify', true, 'Gasto', 17.90, 6, 1, false, 1);
 INSERT INTO GASTO(id,data_pagamento, descricao, fixo, tipo, valor, categoria_id, conta_id,pago, cartao_id) VALUES (5,'2021-05-01', 'Coxinha', true, 'Gasto', 17.90, 1, 1, false, 1);
