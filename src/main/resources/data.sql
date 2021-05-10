c906da4a7186cda4d6503aaceb46ff2e8d115273
GabrielBezerra515 <61557559+GabrielBezerra515@users.noreply.github.com>
2021-05-09 21:13:03 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO USUARIO(data_criacao, email, senha) VALUES('2021-03-01', 'vitormarques@email.com', 'senha@123');
 INSERT INTO CATEGORIA(categoria) VALUES ('Alimento'),('Eletrônicos'),('Casa'),('Locomoção'),('Salário'), ('Entretenimento'), ('Investimento');
 INSERT INTO CONTA(apelido, banco, descricao, numero_conta, usuario_id, valor_atual) VALUES ('C6BANK', 'C6BANK', 'Contra Corrente', '1587389-032',1, 0.0);
-INSERT INTO CARTAO(fechamento,limite,nome,pago,valor,vencimento, usuario_id) VALUES ('2021-03-15', 1000.0,'Cartão de crédito nacional C6Bank', true, 500.0, '2021-04-30',1);
-INSERT INTO META(descricao, meta_gasto, nome, valor_inicial, valor_final,usuario_id) VALUES ('Viagem final de ano', true, 'Viagem', 0.00, 4000.00, 1);
+INSERT INTO CARTAO(fechamento,limite,nome,pago,valor,vencimento, usuario_id) VALUES ('2021-03-15', 1000.0,'C6Bank', true, 500.0, '2021-04-30',1);
+INSERT INTO META(descricao, meta_gasto, nome, valor_inicial, valor_final,usuario_id) VALUES ('Viagem final de ano', false, 'Viagem', 0.00, 4000.00, 1);
 INSERT INTO CONTATO(email, mensagem) VALUES ('usuario@email.com', 'Gostei muito do app de vocês!');
 -- INSERT INTO RENDA(id,data_pagamento, descricao, fixo, tipo, valor, categoria_id, conta_id,recebido) VALUES (1,'2021-03-01', 'Meu salário', true, 'Renda', 2000.00, 5, 1, true);
 -- INSERT INTO RENDA(id,data_pagamento, descricao, fixo, tipo, valor, categoria_id, conta_id,recebido) VALUES (2,'2021-02-01', 'Meu investimento', false, 'Renda', 2000.00, 7, 1, true);
