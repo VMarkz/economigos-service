e851697c4599467b578c18abef2a5098b9556832
VMarkz <vitor.portela@bandtec.com.br>
2021-05-21 20:59:12 -0300

    Ultimos meses retornando mes atual

--- src/main/java/br/com/economigos/service/controller/UsuariosController.java
+++ src/main/java/br/com/economigos/service/controller/UsuariosController.java
@@ -116,7 +116,7 @@ public class UsuariosController {
             List<ValorMensalDto> valorMensalRendasDtos = new ArrayList<>();
             List<ValorMensalTipoDto> valorMensalTipoDtos = new ArrayList<>();
 
-            for (int i = 1; i <= 5; i++) {
+            for (int i = 0; i < 5; i++) {
                 LocalDate localDate = LocalDate.now().minusMonths(i);
                 String anoMes = localDate.toString().substring(0, 7);
                 String mes = localDate.getMonth().getDisplayName(TextStyle.SHORT, Locale.ENGLISH);
