94b1678d001b3a9e49f42a13ecbdeae7fb1ec548
GabrielBezerra515 <61557559+GabrielBezerra515@users.noreply.github.com>
2021-06-10 17:22:43 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -24,7 +24,7 @@ VALUES ('Alimentação', 'G', 'categorias/alimentacao.svg'),
        ('Transporte', 'G', 'categorias/transporte.svg'),
        ('Viagem', 'G', 'categorias/viagem.svg'),
        ('Empréstimos', 'R', 'categorias/emprestimos.svg'),
-       ('Salário', 'R', 'categorias/salrios.svg'),
+       ('Salário', 'R', 'categorias/salarios.svg'),
        ('Outros', 'R', 'categorias/outros-receitas.svg');
 
 INSERT INTO CONTA(apelido, banco, descricao, numero_conta, usuario_id, valor_atual)
