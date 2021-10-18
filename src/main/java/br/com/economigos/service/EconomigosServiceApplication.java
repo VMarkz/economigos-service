927ce7fa3a3f890731ed3c2ed4741c2fdb78417a
VMarkz <vitor.portela@bandtec.com.br>
2021-10-18 20:47:27 -0300

    Add swagger

--- src/main/java/br/com/economigos/service/EconomigosServiceApplication.java
+++ src/main/java/br/com/economigos/service/EconomigosServiceApplication.java
@@ -2,8 +2,10 @@ package br.com.economigos.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class EconomigosServiceApplication {
 
     public static void main(String[] args) {
